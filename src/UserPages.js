2007dd775a403d2097b90e3d95dcac8d5cc5aff4
Helder <he7d3r@gmail.com>
2012-03-11 14:57:05 +0000

    Get the list of results from API and sort by timestamp to make it more practical

--- src/UserPages.js
+++ src/UserPages.js
@@ -1,3 +1,31 @@
+function getUserTools(){
+	var	api = new mw.Api(),
+		user = mw.config.get( 'wgTitle' ).split('/')[0];
+	api.get( {
+		action: 'query',
+		list: 'search',
+		srsearch: '(intitle:.js OR intitle:.css) prefix:User:' + user,
+		srnamespace: 2,
+		srprop: 'timestamp',
+		srlimit: 100
+	}, {
+		ok: function ( data ) {
+			var list = (data && data.query && data.query.search) || [];
+			list.sort( function(a,b){
+				return (new Date(b.timestamp) - new Date(a.timestamp) );
+			});
+			$.each( data && data.query && data.query.search, function( id, result ){
+				// Add a link to list the scripts of the current user
+				mw.util.addPortletLink(
+					'p-js-list',
+					mw.util.wikiGetlink( result.title ),
+					result.title.replace( new RegExp( '^.+?' + user + '\\/' ), '' )
+				);
+			} );
+		}
+	} );
+}
+
 /**
  * Customization for user pages
  */
@@ -5,14 +33,22 @@ if ( $.inArray( mw.config.get( 'wgNamespaceNumber' ), [ 2, 3 ]) > -1
 	&& $.inArray( mw.config.get( 'wgAction' ), [ 'view', 'purge' ]) > -1
 ) {
 	$(function () {
+		// Create a new portlet for user scripts
+		$('#p-cactions').clone().prepend('<h4>JS</h4>')
+		.insertAfter('#p-namespaces').attr({
+			'id': 'p-js-list',
+			'class': 'vectorMenu emptyPortlet'
+		}).find('li').remove().end().find('span').text('JS List');
 		// Add a link to list the scripts of the current user
-		mw.util.addPortletLink(
-			'p-namespaces',
-			mw.util.wikiGetlink( 'Special:Search/intitle:js prefix:User:' + mw.config.get( 'wgTitle' ).split('/')[0]) + '?limit=500',
-			'JS',
-			'ca-user-js',
-			'Pesquisar scripts deste editor'
-		);
+		$(mw.util.addPortletLink(
+			'p-js-list',
+			'#',
+			'Obter lista...'
+		) ).click( function( e ){
+			e.preventDefault();
+			mw.loader.using( 'mediawiki.api', getUserTools);
+		});
+
 		if ( mw.config.get( 'wgTitle' ).indexOf( mw.config.get( 'wgUserName' ) ) === -1 ) {
 			var html = $('#firstHeading').html();
 			// Restore original title of user pages
